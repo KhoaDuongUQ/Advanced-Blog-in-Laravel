88258384fd53db7ef81c57e022fb7c35db71adbf
Anh Khoa (Forrest) <duonganhkhoa95@gmail.com>
2018-08-18 13:41:36 +1000

    Configure JS and CSS for the app, set up database config settings

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,6 +1,4 @@
-
 window._ = require('lodash');
-window.Popper = require('popper.js').default;
 
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
@@ -11,7 +9,6 @@ window.Popper = require('popper.js').default;
 try {
     window.$ = window.jQuery = require('jquery');
 
-    require('bootstrap');
 } catch (e) {}
 
 /**
